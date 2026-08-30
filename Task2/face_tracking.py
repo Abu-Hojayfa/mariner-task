643fca2b231b6c02bef6cc2a39ae8d332c8c2322
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2026-08-31 03:13:51 +0600

    added model for face track and recognise and success to do it

--- Task2/face_tracking.py
+++ Task2/face_tracking.py
@@ -1,1 +1,24 @@
-print("hello from face tracking module")
+import cv2 as cv
+from ultralytics import YOLO
+
+model = YOLO("model/yolov8m-face.pt")
+
+vid = cv.VideoCapture("data/test.mp4")
+
+if not vid.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+
+while cv.waitKey(1) != ord("x"):
+    ret, frame = vid.read()
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+    result = model(frame,  verbose=False)
+    cv.imshow("Face Detection", result[0].plot())
+
+
+vid.release()
+
+cv.destroyAllWindows()
