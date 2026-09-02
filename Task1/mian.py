a10fbda887903f89c2be80c4bcb55d27fedb9314
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2026-09-03 05:03:39 +0600

    add ball and gravity

--- Task1/mian.py
+++ Task1/mian.py
@@ -2,23 +2,32 @@ import sys
 import pygame
 
 # pygame setup
+width, height = 1280, 720
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Floppy Ball")
 clock = pygame.time.Clock()
+
+ball_x = 80
+ball_y = height // 2
+ball_radius = 20
+ball_velocity = 0
+gravity = 0.5
+
+
 running = True
-
-
-
 while running:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((135, 206, 235))
 
-    
+    ball_velocity += gravity
+    ball_y += ball_velocity
+
+    screen.fill((135, 206, 235))
+    pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), ball_radius)    
     pygame.display.flip() 
     dt = clock.tick(60)
 
